daa9f2a0535849e9d203dfbe9746a26078e4cdae
sarafotros <sarafotros@gmail.com>
2020-07-23 19:34:02 +0100

    functions added issue #1: pressing equle few times will break the app issue#2 operatos should be add once

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,66 +1,173 @@
 import React from 'react';
 import { Component } from 'react';
-
+import { evaluate } from 'mathjs';
 import './Calculator.css';
 
 export default class Calculator extends Component {
+  state = {
+    screenShow: '',
+  };
+
+  buttonPressHandler = (val) => {
+    this.setState({
+      screenShow: this.state.screenShow + val,
+    });
+  };
+
+  handleOperate = (val) => {
+    this.setState({
+      screenShow: this.state.screenShow + val,
+    });
+  };
+  clearInput = () => {
+    this.setState({
+      screenShow: '',
+    });
+  };
+
+  handleEqual = () => {
+    this.setState({
+      screenShow: evaluate(this.state.screenShow),
+    });
+  };
+
   render() {
     return (
       <div className='calculator'>
-        <input type='text' class='calculator-screen' value='0' disabled />
+        <input
+          type='text'
+          class='calculator-screen'
+          value={this.state.screenShow}
+          disabled
+        />
 
         <div class='calculator-keys'>
-          <button type='button' class='operator' value='+'>
+          <button
+            type='button'
+            class='operator'
+            value='+'
+            onClick={(e) => this.buttonPressHandler(e.target.value)}
+          >
             +
           </button>
-          <button type='button' value='7'>
+          <button
+            type='button'
+            value='7'
+            onClick={(e) => this.buttonPressHandler(e.target.value)}
+          >
             7
           </button>
-          <button type='button' value='8'>
+          <button
+            type='button'
+            value='8'
+            onClick={(e) => this.buttonPressHandler(e.target.value)}
+          >
             8
           </button>
-          <button type='button' value='9'>
+          <button
+            type='button'
+            value='9'
+            onClick={(e) => this.buttonPressHandler(e.target.value)}
+          >
             9
           </button>
-          <button type='button' class='operator' value='-'>
+          <button
+            type='button'
+            class='operator'
+            value='-'
+            onClick={(e) => this.buttonPressHandler(e.target.value)}
+          >
             -
           </button>
-          <button type='button' value='4'>
+          <button
+            type='button'
+            value='4'
+            onClick={(e) => this.buttonPressHandler(e.target.value)}
+          >
             4
           </button>
-          <button type='button' value='5'>
+          <button
+            type='button'
+            value='5'
+            onClick={(e) => this.buttonPressHandler(e.target.value)}
+          >
             5
           </button>
-          <button type='button' value='6'>
+          <button
+            type='button'
+            value='6'
+            onClick={(e) => this.buttonPressHandler(e.target.value)}
+          >
             6
           </button>
-          <button type='button' class='operator' value='*'>
+          <button
+            type='button'
+            class='operator'
+            value='*'
+            onClick={(e) => this.buttonPressHandler(e.target.value)}
+          >
             &times;
           </button>
-          <button type='button' value='1'>
+          <button
+            type='button'
+            value='1'
+            onClick={(e) => this.buttonPressHandler(e.target.value)}
+          >
             1
           </button>
-          <button type='button' value='2'>
+          <button
+            type='button'
+            value='2'
+            onClick={(e) => this.buttonPressHandler(e.target.value)}
+          >
             2
           </button>
-          <button type='button' value='3'>
+          <button
+            type='button'
+            value='3'
+            onClick={(e) => this.buttonPressHandler(e.target.value)}
+          >
             3
           </button>
-          <button type='button' class='operator' value='/'>
+          <button
+            type='button'
+            class='operator'
+            value='/'
+            onClick={(e) => this.buttonPressHandler(e.target.value)}
+          >
             &divide;
           </button>
 
-          <button type='button' value='0'>
+          <button
+            type='button'
+            value='0'
+            onClick={(e) => this.buttonPressHandler(e.target.value)}
+          >
             0
           </button>
-          <button type='button' class='decimal' value='.'>
+          <button
+            type='button'
+            class='decimal'
+            value='.'
+            onClick={(e) => this.buttonPressHandler(e.target.value)}
+          >
             .
           </button>
-          <button type='button' class='all-clear' value='all-clear'>
+          <button
+            type='button'
+            class='all-clear'
+            value='all-clear'
+            onClick={this.clearInput}
+          >
             CLEAR
           </button>
 
-          <button type='button' class='equal-sign' value='='>
+          <button
+            type='button'
+            class='equal-sign'
+            value='='
+            onClick={this.handleEqual}
+          >
             =
           </button>
         </div>
